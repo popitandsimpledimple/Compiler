b5c3cdf261424e77672cd929669a3ec3fcb55d50
popitandsimpledimple <105473707+popitandsimpledimple@users.noreply.github.com>
2022-11-09 12:37:14 +1000

    Update Program.cs

--- Compiler_1/Compiler_1/Program.cs
+++ Compiler_1/Compiler_1/Program.cs
@@ -1,10 +1,15 @@
-﻿var sr = new StreamReader("C:/Users/Mixa/Desktop/Compiler_1/Test.txt");
+using Lexer;
+
+var sr = new StreamReader("C:/Users/Mixa/Desktop/Compiler_1/Test.txt");
 var lexer = new Lexer.Lexer(sr);
 
-for (; ; )
+for (;;)
 {
     var token = lexer.NextToken();
+    if (token.TokenType == TokenType.EOF)
+    {
+        Console.WriteLine(new Token(TokenType.EOF, ' ', ""));
+        break;
+    }
     Console.WriteLine(token);
-    break;
-
 }
