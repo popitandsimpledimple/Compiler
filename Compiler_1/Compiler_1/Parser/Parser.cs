a335ef73891b41b9b9de2cd1f503ad95594f7bc8
popitandsimpledimple <105473707+popitandsimpledimple@users.noreply.github.com>
2022-11-19 20:48:14 +1000

    Add files via upload

---
+++ Compiler_1/Compiler_1/Parser/Parser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lexer;
+using static Lexer.Token;
+
+namespace Compiler_1.Parser
+{
+    public class Parser
+    {
+       
+        private Token _readToken;
+        private string _buffer;
+
+        public Parser(Token readToken, string buffer)
+        {
+            _readToken = readToken;
+            _buffer = readToken.NextToken();
+        }
+
+
+
+    }
+}
